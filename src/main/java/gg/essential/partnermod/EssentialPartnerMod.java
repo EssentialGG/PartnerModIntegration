9ba089fa45df52ec945fc22fa10434bf71d5864e
DJtheRedstoner <52044242+DJtheRedstoner@users.noreply.github.com>
2025-04-29 00:19:25 -0400

    Add copyright headers

--- src/main/java/gg/essential/partnermod/EssentialPartnerMod.java
+++ src/main/java/gg/essential/partnermod/EssentialPartnerMod.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2025 ModCore Inc. All rights reserved.
+ *
+ * This code is part of ModCore Inc.’s Essential Partner Mod Integration
+ * repository and is protected under copyright. For the full license, see:
+ * https://github.com/EssentialGG/EssentialPartnerMod/tree/main/LICENSE
+ *
+ * You may modify, fork, and use the Mod, but may not retain ownership of
+ * accepted contributions, claim joint ownership, or use Essential’s trademarks.
+ */
+
 // Stub class so Forge doesn't complain that there's no mod for the toml entry when running standalone jars for testing
 // but at the same time it can easily be excluded so Forge doesn't complain about there being no toml entry for the mod
 // when bundled in another mod jar.
--- versions/1.16.5-forge/src/main/java/gg/essential/partnermod/MixinPlugin.java
+++ versions/1.16.5-forge/src/main/java/gg/essential/partnermod/MixinPlugin.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2025 ModCore Inc. All rights reserved.
+ *
+ * This code is part of ModCore Inc.’s Essential Partner Mod Integration
+ * repository and is protected under copyright. For the full license, see:
+ * https://github.com/EssentialGG/EssentialPartnerMod/tree/main/LICENSE
+ *
+ * You may modify, fork, and use the Mod, but may not retain ownership of
+ * accepted contributions, claim joint ownership, or use Essential’s trademarks.
+ */
+
 package gg.essential.partnermod;
 
 import gg.essential.partnermod.loader.EssentialPartnerLoader;
